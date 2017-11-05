1996e204e08282de64f573ac258ffdeecc784e22
JPMinoli <gpminoli@gmail.com>
2017-11-05 00:22:39 -0500

    Se generan los archivos iniciales.

---
+++ Linsytion.f90
@@ -0,0 +1,14 @@
+!---------------------------Header-------------------------------!
+! MODULE Lynsition
+! Libreria que contiene subrutinas para solución de sistemas de
+! ecuaciones lineales.
+! 
+! AUTOR: Juan Pablo Velásquez Minoli  
+!-------------------------End Header-----------------------------!
+
+MODULE Lynsition
+
+SUBROUTINE conjugateGradient
+
+END SUBROUTINE
+END MODULE
